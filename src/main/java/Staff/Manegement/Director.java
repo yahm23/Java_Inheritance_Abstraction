859d4086f059c0faf19643f5c7daaa1d0b841200
Yousef <yahmed1996a@gmail.com>
2020-01-13 14:50:33 +0000

    adds budget getter method

--- src/main/java/Staff/Manegement/Director.java
+++ src/main/java/Staff/Manegement/Director.java
@@ -1,4 +1,15 @@
 package Staff.Manegement;
 
-public class Director {
+public class Director extends Manager {
+
+    double budget;
+
+    public Director(String name, String ni, double salary, String dept, double budget) {
+        super(name, ni, salary, dept);
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
 }
--- src/test/java/DirectorTest.java
+++ src/test/java/DirectorTest.java
@@ -1,2 +1,28 @@
+
+import Staff.Manegement.Director;
+import Staff.Manegement.Manager;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class DirectorTest {
+
+    Director director;
+
+    @Before
+    public void setup(){
+        director = new Director("Morgaine","1234",28000.00,"Management", 100000.00);
+    }
+
+
+    @Test
+    public void canGetBudget(){
+        assertEquals(100000.00, director.getBudget(), 0.001);
+    }
+
+
+
+
+
 }
