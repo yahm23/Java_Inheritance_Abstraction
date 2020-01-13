76656850d75d38a54dbfbabe5a372456a9a061a1
Yousef <yahmed1996a@gmail.com>
2020-01-13 14:43:42 +0000

    Manager  tests passing

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -17,4 +17,36 @@ public class ManagerTest {
     public void canGetDept(){
         assertEquals("HR",manager.getDept());
     }
+
+    @Test
+    public void canManagerSalary(){
+        assertEquals(26000,manager.getSalary(),0.001);
+    }
+
+    @Test
+    public void employeeHasName(){
+        assertEquals("Yousef",manager.getName());
+    }
+
+    @Test
+    public void employeeHasNi(){
+        assertEquals("1234",manager.getNi());
+    }
+
+    @Test
+    public void employeeHasSalary(){
+        assertEquals(26000.00,manager.getSalary(),0.001);
+    }
+
+    @Test
+    public void employeeHasSalaryIncrease(){
+        manager.raiseSalary(1000.00);
+        assertEquals(27000.00,manager.getSalary(),0.001);
+    }
+
+    @Test
+    public void employeeCanBePaidBonus(){
+
+        assertEquals(260.00, manager.getBonus(),0.001);
+    }
 }
